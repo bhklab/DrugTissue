fd5b72049d35df201f577068f0b30ee9744e6462
Fupan Yao <gosuzombie@opennet-33-94.uhnres.utoronto.ca>
2016-10-24 11:50:16 -0400

    added openxlsx

--- startup.R
+++ startup.R
@@ -1,6 +1,6 @@
 source("https://bioconductor.org/biocLite.R")
 .libPaths("/")
-list.of.packages <- c("ggplot2", "XML", "mgcv", "reshape2", "grid", "gridExtra", "gplots", "pheatmap","RColorBrewer")
+list.of.packages <- c("ggplot2", "XML", "mgcv", "reshape2", "grid", "gridExtra", "gplots", "pheatmap","RColorBrewer", "openxlsx")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, repos='http://cran.utstat.utoronto.ca/')
 
@@ -24,5 +24,11 @@ if(!require(survcomp))
 if(!require(piano))
 {
   biocLite("piano")
+  library(piano)
+}
+
+if(!require(PharmacoGx))
+{
+  biocLite("PharmacoGx")
   library(piano)
 }
