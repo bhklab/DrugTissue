6756bf7fcb0af4b6a6f7af01bb3454ab51f3af31
Fupan Yao <gosuzombie@opennet-33-94.uhnres.utoronto.ca>
2017-03-08 17:03:49 -0500

    minor edits

--- TEA_dependency_installer.R
+++ TEA_dependency_installer.R
@@ -2,7 +2,7 @@ source("https://bioconductor.org/biocLite.R")
 
 list.of.CRAN.packages <- c("parallel", "ggplot2", "XML", "mgcv", "reshape2", "grid", "gridExtra", "gplots","pheatmap","RColorBrewer", 
                       "VennDiagram", "gdata", "e1071", "xtable", "data.table", "snowfall", "utils",
-                      "Hmisc", "WriteXLS", "rgl", "qpcR", "devtools", "ggplot2")
+                      "Hmisc", "xlsx", "rgl", "qpcR", "devtools", "ggplot2")
 new.CRAN.packages <- list.of.CRAN.packages[!(list.of.CRAN.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.CRAN.packages, repos='http://cran.utstat.utoronto.ca/', dependencies = TRUE, clean = TRUE, Ncpus = 2, verbose = TRUE, quiet = TRUE)
 
@@ -18,6 +18,3 @@ message("=== package install success status ===")
 for(package in dependencies){
   message(paste(package, library(package, character.only = TRUE, quietly = TRUE, logical.return = TRUE, verbose = FALSE), sep = ": "))
 }
-
-### Write.xls support for XLSX
-installXLSXsupport()
