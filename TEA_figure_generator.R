0861845106874ffedeb9809d6e1d30b57a1e13b0
Fupan Yao <f.yao@mail.utoronto.ca>
2018-01-13 12:00:07 -0500

    legacy code fixes

--- TEA_figure_generator.R
+++ TEA_figure_generator.R
@@ -24,84 +24,44 @@ CTRPv2 <- downloadPSet("CTRPv2")
 if(!dir.exists("./figures")){
   dir.create("./figures")
 }
+
+
 ### Cell Line Breakdown by Dataset (Figure 2A)
 
-linenums <- as.data.frame(matrix(ncol = 4, nrow = nrow(combined1)))
-rownames(linenums) <- unique(c(p_adj_false$Tissue, p_adj_true$Tissue))
-colnames(linenums) <- c("CCLE", "GDSC1000", "CTRPv2", "gCSI")
+linenums <- data.frame()
+
+t <- table(CCLE@cell$tissueid)
+linenums[names(t), "CCLE"] <- t
+
+t <- table(GDSC1000@cell$tissueid)
+linenums[names(t), "GDSC1000"] <- t
+
+t <- table(gCSI@cell$tissueid)
+linenums[names(t), "gCSI"] <- t
+
+t <- table(CTRPv2@cell$tissueid)
+linenums[names(t), "CTRPv2"] <- t
+
 linenums[is.na(linenums)] <- 0
+linenums <- linenums[rownames(linenums) != "",]
 
-for(y in 1:nrow(CCLE@cell))
-{
-  if(length(grep(CCLE@cell$cellid[y], rownames(na.omit(CCLE@sensitivity$profiles)))) > 0)
-  {
-    if(!is.na(CCLE@cell$tissueid[y]))
-    {
-      linenums[CCLE@cell$tissueid[y], "CCLE"] <- linenums[CCLE@cell$tissueid[y], "CCLE"] + 1
-    }
-  }
-}
-for(y in 1:nrow(GDSC1000@cell))
-{
-  if(length(grep(GDSC1000@cell$cellid[y], rownames(na.omit(GDSC1000@sensitivity$profiles)))) > 0)
-  {
-    if(!is.na(GDSC1000@cell$tissueid[y]))
-    {
-      if(GDSC1000@cell$tissueid[y] == "esophagus")
-      {
-        linenums["oesophagus", "GDSC1000"] <- linenums["oesophagus", "GDSC1000"] + 1
-      }
-      else
-      {
-        linenums[GDSC1000@cell$tissueid[y], "GDSC1000"] <- linenums[GDSC1000@cell$tissueid[y], "GDSC1000"] + 1
-      }
-    }
-  }
-}
-for(y in 1:nrow(CTRPv2@cell))
-{
-  if(length(grep(CTRPv2@cell$cellid[y], rownames(na.omit(CTRPv2@sensitivity$profiles)))) > 0)
-  {
-    if(!is.na(CTRPv2@cell$tissueid[y]))
-    {
-      linenums[CTRPv2@cell$tissueid[y], "CTRPv2"] <- linenums[CTRPv2@cell$tissueid[y], "CTRPv2"] + 1
-    }
-  }
-}
-for(y in 1:nrow(gCSI@cell))
-{
-  if(length(grep(gCSI@cell$unique.id[y], rownames(na.omit(gCSI@sensitivity$profiles)))) > 0)
-  {
-    if(!is.na(gCSI@cell$tissueid[y]))
-    {
-      linenums[gCSI@cell$tissueid[y], "gCSI"] <- linenums[gCSI@cell$tissueid[y], "gCSI"] + 1
-    }
-  }
-}
-
-
-linenums <- linenums[grep("breast_", rownames(linenums)) * -1, ]
-linenums <- na.omit(linenums)
-
-for(x in 1:nrow(linenums))
-{
-  linenums[x, "median"] <- median(as.numeric(linenums[x, 1:4]))
-}
-
+linenums$median <- apply(linenums, MARGIN = 1, median)
+linenums$sum <- apply(linenums, MARGIN = 1, sum)
+linenums <- linenums[linenums$sum > 8,]
 linenums <- linenums[order(linenums$median), ]
 
-linenums <- linenums[, -5]
+linenums <- linenums[, c(-5, -6)]
 order <- rownames(linenums)
 
-linenums <- melt(as.matrix(linenums))
+linenums_melt <- melt(as.matrix(linenums))
 
-colnames(linenums) <- c("tissue type", "dataset", "value")
+colnames(linenums_melt) <- c("tissue type", "dataset", "value")
 
-linenums$`tissue type` <- gsub("_", " ", linenums$`tissue type`)
-linenums$`tissue type` <- as.factor(linenums$`tissue type`)
+linenums_melt$`tissue type` <- gsub("_", " ", linenums_melt$`tissue type`)
+linenums_melt$`tissue type` <- as.factor(linenums_melt$`tissue type`)
 
-pdf("./figures/figure2a.pdf")
-ggplot(linenums, aes(x = linenums$`tissue type`, y = linenums$value, fill=linenums$dataset)) + 
+pdf(file.path(figure_output, "figure2a.pdf"))
+ggplot(linenums_melt, aes(x = linenums_melt$`tissue type`, y = linenums_melt$value, fill=linenums_melt$dataset)) + 
   geom_bar(stat = "identity", position="dodge") + 
   theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 10), axis.text.y = element_text(size = 10), axis.title=element_text(size=10)) +
   guides(fill=guide_legend(title="")) +
@@ -109,7 +69,6 @@ ggplot(linenums, aes(x = linenums$`tissue type`, y = linenums$value, fill=linenu
   labs(x = NULL, y = "number of cell lines") +
   theme(legend.text=element_text(size=10))
 dev.off()
-
 
 ### Venn Diagrams for Overlap (Figure 2B)
 
